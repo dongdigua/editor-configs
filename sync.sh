d5f0a36277bddb58381723b61caa67c6ab29ac8f
dongdigua <dongdigua@outlook.com>
2022-08-09 20:39:14 +0800

    rofi config

--- sync.sh
+++ sync.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
-cp ~/.vimrc .
-cp ~/.emacs .
-cp ~/.tmux.conf .
-cp ~/.nethackrc .
-cp ~/.config/sway/* sway/
-cp ~/.config/waybar/* waybar/
+cp ~/.vimrc              .
+cp ~/.emacs              .
+cp ~/.tmux.conf          .
+cp ~/.nethackrc          .
+cp ~/.config/sway/*      sway/
+cp ~/.config/waybar/*    waybar/
+cp ~/.config/swaylock/*  swaylock/
+cp ~/.config/rofi/*      rofi/
 # cp ~/.config/wayfire.ini .
