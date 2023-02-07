7de0fc0cf8e89ff3094584131f12a729c5b6b6eb
digua <digua@laptop.my.domain>
2023-02-07 23:09:45 +0800

    add muttrc, openbsd use gentoo PS1

--- OpenBSD/.profile
+++ OpenBSD/.profile
@@ -4,9 +4,11 @@
 export PATH=$PATH:$HOME/bin
 export HISTFILE=$HOME/.ksh_hist
 export HISTCONTROL=ignoredup
-export PS1='\u@\h \w \$ '
-export VISUAL=vi
+#export VISUAL=vi
 export AUTOCONF_VERSION=2.71
+export CLICOLOR=1
+# https://gitweb.gentoo.org/repo/gentoo.git/tree/app-shells/bash/files/bashrc
+export PS1='\[\033]0;\u@\h:\w\007\]\[\033[01;32m\]\u@\h\[\033[01;34m\] \w \$\[\033[00m\] '
 
 alias vim=nvim
 alias e='emacs -nw'
--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 cp ~/.emacs                            .
 cp ~/.tmux.conf                        .
 cp ~/.nethackrc                        .
@@ -6,11 +6,12 @@ cp ~/.config/starship.toml             .
 cp ~/.config/libinput-gestures.conf    .
 cp ~/.config/nvim/init.vim             .
 cp ~/.config/fish/config.fish          .
+cp ~/.config/mutt/muttrc               .
 cp -r ~/.config/sway/*                 sway/
 cp -r ~/.config/waybar/*               waybar/
 cp ~/.config/rofi/*                    rofi/
 cp ~/.config/qutebrowser/config.py     qutebrowser/
 cp ~/.config/qutebrowser/home.html     qutebrowser/
-cp ~/.config/qutebrowser/home.html     ~/Documents/
-sed -i "s/duckduckgo/bing/g"           ~/Documents/home.html
+#cp ~/.config/qutebrowser/home.html     ~/Documents/
+#sed -i "s/duckduckgo/bing/g"           ~/Documents/home.html
 elixir epp.ex .emacs dump >            ~/.emacs.d/emacs/lisp/site-init.el
